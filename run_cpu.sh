5a1c99fc57848d8f2f961daa80a741e90af336be
Stephen <7speter@gmail.com>
2025-11-20 18:02:06 -0500

    Updates the run_cpu script with experiments

--- run_cpu.sh
+++ run_cpu.sh
@@ -6,9 +6,24 @@ fi
 
 INPUT="$1"
 OUTPUT="${2:-output.png}"
+HOST_BASE_PATH="/opt/container-data/whisper-app"
+
+#Only keep the filename, no host path
+BASENAME=$(basename "$INPUT")
+
+#Require the file to EXIST inside ./input/
+if [ ! -f "input/$BASENAME" ]; then
+	echo "Error: File input/$BASENAME not found."
+	echo "Put your audio file into the ./input/ directory first"
+	exit 1
+fi
+
+#Use the full path for the audio file on the HOST machine
+HOST_AUDIO_PATH="$(pwd)/input/$BASENAME"
 
 podman run --rm \
-  -v $(pwd)/input:/app/input \
-  -v $(pwd)/output:/app/output \
+  -v $HOST_BASE_PATH/input:/app/input:Z \
+  -v $HOST_BASE_PATH/output:/app/output:Z \
+  --user $(id -u):$(id -g) \
   whispered-diffusion:cpu \
-  "/app/input/$INPUT" --out-image "/app/output/$OUTPUT"
+  "/app/input/$BASENAME" --out-image "/app/output/$OUTPUT"
